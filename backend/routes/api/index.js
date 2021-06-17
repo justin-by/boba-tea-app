fee0fa318f38a375d78cd20fb16c8229af01ffc2
justin <flaminlinkvp@gmail.com>
2021-06-17 12:41:16 -0700

    user login api route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,15 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
+const asyncHandler = require("express-async-handler");
+const {
+  setTokenCookie,
+  restoreUser,
+  requireAuth,
+} = require("../../utils/auth.js");
+const { User } = require("../../db/models");
 
-router.post('/test', function(req, res) {
-    res.json({
-        requestBody: req.body
-    });
-})
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 
 module.exports = router;
